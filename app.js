04b5f06e8cd0f799cfc1fd1a89ef6c62926aa2ff
ibrahim <ibba2004@gmail.com>
2020-07-06 21:33:05 +0530

    Published on heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const bodyParser=require('body-parser');
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
