fa1eb40f484420219389e4f79a81cca07b549f98
ibrahim <ibba2004@gmail.com>
2020-07-07 22:39:39 +0530

    Some modifications

--- app.js
+++ app.js
@@ -1,33 +1,61 @@
-const express=require("express");
-const app=express();
-const bodyParser=require('body-parser');
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
 
 app.listen(process.env.PORT || 3000);
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-var tasks=[];
+var tasks = [];
+var worktasks = [];
 
-app.get("/",function(req,res){
-   var date=new Date();
-   options={
-     day:"numeric",
-     weekday:"long",
-     month:"long",
-   }
-   var todaysDate=date.toLocaleDateString("en-US",options);
-  res.render("index",{todaysDate:todaysDate,tasks:tasks});
+app.get("/", function(req, res) {
+  var date = new Date();
+  options = {
+    day: "numeric",
+    weekday: "long",
+    month: "long"
+  }
+  var todaysDate = date.toLocaleDateString("en-US", options);
+  res.render("index", {
+    listTitle: todaysDate,
+    tasks: tasks
+  });
 });
 
-app.post("/addItem",function(req,res){
-var newItem=req.body.newItem;
-tasks.push(newItem);
-res.redirect("/");
+
+app.get("/work", function(req, res) {
+  res.render("index", {
+    listTitle: "Work",
+    tasks: worktasks
+  });
 });
 
-app.post("/removeItem",function(req,res){
-  var removedItemNumber= req.body.item;
-     tasks.splice(removedItemNumber,1);
-     res.redirect("/");
+app.post("/addItem", function(req, res) {
+  var newItem = req.body.newItem;
+  console.log(req.body);
+
+  if (req.body.button ==="Work"){
+    worktasks.push(newItem);
+    res.redirect("/work");
+  } else {
+    tasks.push(newItem);
+    res.redirect("/");
+  }
+});
+
+app.post("/removeItem", function(req, res) {
+  var removedItemNumber = req.body.item;
+  if(req.body.rm=="Work"){
+    worktasks.splice(removedItemNumber,1);
+   res.redirect("/work");
+  }
+  else{
+  tasks.splice(removedItemNumber, 1);
+  res.redirect("/");
+}
+
 });
